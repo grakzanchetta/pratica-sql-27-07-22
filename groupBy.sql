44f09339314b0401c090ba9f27ba5301aabe837f
Gabriel Rak <grakzanchetta@gmail.com>
2022-07-28 13:39:00 -0300

    segundo exercício

--- groupBy.sql
+++ groupBy.sql
@@ -2,3 +2,6 @@
 --Exercício 1
 
 SELECT COUNT ("endDate") as "currentExperiences" FROM experiences;
+
+--Exercício 2
+SELECT "userId" AS id, COUNT ("userId") AS educations FROM educations GROUP BY "userId";
