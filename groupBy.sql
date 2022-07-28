87e85b8e7c5cce765264ac38c9e811c0b7bc3d7c
Gabriel Rak <grakzanchetta@gmail.com>
2022-07-28 13:52:26 -0300

    terceiro exercício

--- groupBy.sql
+++ groupBy.sql
@@ -1,7 +1,14 @@
 --Prática GroupBy dia 27-07-22
---Exercício 1
 
+--Exercício 1
 SELECT COUNT ("endDate") as "currentExperiences" FROM experiences;
 
 --Exercício 2
 SELECT "userId" AS id, COUNT ("userId") AS educations FROM educations GROUP BY "userId";
+
+--Exercicio 3
+SELECT user.name AS writer, COUNT (testimonials."recitipentId") as "testimonialCount"
+FROM testimonials
+JOIN users ON testimonials."writerId" = users.id
+WHERE users.id = 435
+GROUP BY writer;
